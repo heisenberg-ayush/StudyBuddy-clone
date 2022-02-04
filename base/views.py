fa0d346f5861a3711992c80c4bb7b138f236e11b
Ayush Pandey <73249644+heisenberg-ayush@users.noreply.github.com>
2022-02-05 02:20:25 +0530

    Mobile Responsive

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
@@ -65,7 +66,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
         )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
     
@@ -175,3 +176,16 @@ def updateUser(request):
     
     context = {'form':form}
     return render(request, 'base/update-user.html', context)
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {'topics':topics}
+    return render(request, 'base/topics.html', context)
+
+def activityPage(request):
+    room_messages = Message.objects.all()[0:3]
+
+    context = {'room_messages':room_messages}
+    return render(request, 'base/activity.html', context)
